7b609acb6dd9f39b32b0da234cde3f45cd860708
Rachid <81814923+R4ch1d-3xyp3ry@users.noreply.github.com>
2021-04-12 17:49:01 +0200

    fix : functions generate/repeat && css

--- functions.js
+++ functions.js
@@ -1,11 +1,39 @@
 console.log("test");
 console.log(document.getElementById('iptValider'));
-document.getElementById('iptValider').addEventListener('click', generate);
+document.getElementById('iptValider').addEventListener('click', repeat);
 
-function generate() {
-    console.log('Bouton valider !');
-    let txtAreaValue = document.getElementsByTagName('textarea')[0].value;
-    console.log('test 2 ' + txtArea);
+// Repeat the string or character
+function repeat() 
+{
+		console.log('Bouton valider !');
+		// Récupération du texte
+		let txtArea = document.getElementById('txtAreaId');
+		let areaValue = null;
+		// Récupération du compteur
+		let counter = document.getElementById('countId');
+		let counterValue = undefined;
+		// Valeur à afficher
+		let retValue = "";
+
+		console.log("1-cpt :" + counter.value);
+
+		areaValue = ( txtArea == null || txtArea == undefined || txtArea.value === "" ) ? "" : txtArea.value;
+		counterValue = ( counter == null || counter == undefined || counter.value === "" ) ? 0 : counter.value;
+		
+		
+		console.log("2-cpt:"+counterValue);
+		console.log("area :" + areaValue);
+
+		for(let i=0; i<counterValue; i++)
+		{
+			retValue += areaValue;
+		}
+		console.log("retValue : " + retValue);
+
+		if ( retValue.length != 0 )
+		{
+			txtArea.value = retValue;
+		}
 }
 
 
@@ -16,12 +44,12 @@ function generate() {
 * src : https://stackoverflow.com/questions/8796988/binding-multiple-events-to-a-listener-without-jquery
 */
 function addListenerMulti(element, eventNames, listener) {
-    var events = eventNames.split(' ');
-    for (var i=0, iLen=events.length; i<iLen; i++) {
-      element.addEventListener(events[i], listener, false);
-    }
-  }
-  
+		var events = eventNames.split(' ');
+		for (var i=0, iLen=events.length; i<iLen; i++) {
+			element.addEventListener(events[i], listener, false);
+		}
+	}
+	
 addListenerMulti(window, 'keyup keypress', countCharacter);
 
 
@@ -30,6 +58,6 @@ addListenerMulti(window, 'keyup keypress', countCharacter);
 
 function countCharacter()
 {
-    var lng = document.getElementsByTagName('textarea')[0].value.length;
+		var lng = document.getElementsByTagName('textarea')[0].value.length;
 	document.getElementById("charcount").innerHTML = lng + ' out of 400 characters';
 }
