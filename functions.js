1e948a37900c998ef615b15e2fb7f818b5d76761
Rachid <81814923+R4ch1d-3xyp3ry@users.noreply.github.com>
2021-04-21 20:35:44 +0200

    fix : index functions

--- functions.js
+++ functions.js
@@ -1,59 +1,49 @@
-console.log("test");
-console.log(document.getElementById('iptValider'));
-
-document.getElementById('iptValider').addEventListener('click', repeat);
-
 // Repeat the string or character
-function repeat() 
+function repeat()
 {
 		console.log('Bouton valider !');
 		// Récupération du texte
-		/*let txtArea = document.getElementById('txtAreaId');
+		let txtArea = document.getElementById('txtAreaId');
 		let areaValue = null;
 		// Récupération du compteur
 		let counter = document.getElementById('countId');
-		let counterValue = undefined;
+		let counterValue = null;
 		// Valeur à afficher
 		let retValue = "";
 
 		console.log("1-cpt :" + counter.value);
 
-		areaValue = ( txtArea == null || txtArea == undefined || txtArea.value === "" ) ? "" : txtArea.value;
+		areaValue = ( txtArea == null || txtArea == undefined ) ? "" : txtArea.value;
 		counterValue = ( counter == null || counter == undefined || counter.value === "" ) ? 0 : counter.value;
 		
-		
-		console.log("2-cpt:"+counterValue);
-		console.log("area :" + areaValue);
-
+		console.log("2-area :" + areaValue);
+		console.log("2-cpt :" + counterValue);
 		for(let i=0; i<counterValue; i++)
 		{
 			retValue += areaValue;
 		}
-		console.log("retValue : " + retValue);
 
 		if ( retValue.length != 0 )
 		{
-			txtArea.value = retValue;
-		}*/
+			console.log("yes : " + retValue)
+			document.getElementById('txtAreaId').value = retValue;
+		}
 }
 
 
-/* Add one or more listeners to an element
-** @param {DOMElement} element - DOM element to add listeners to
-** @param {string} eventNames - space separated list of event names, e.g. 'click change'
-** @param {Function} listener - function to attach for each event as a listener
-* src : https://stackoverflow.com/questions/8796988/binding-multiple-events-to-a-listener-without-jquery
+/* 
+	Add one or more listeners to an element
+	** @param {DOMElement} element - DOM element to add listeners to
+	** @param {string} eventNames - space separated list of event names, e.g. 'click change'
+	** @param {Function} listener - function to attach for each event as a listener
+	* src : https://stackoverflow.com/questions/8796988/binding-multiple-events-to-a-listener-without-jquery
 */
 function addListenerMulti(element, eventNames, listener) {
-		var events = eventNames.split(' ');
-		for (var i=0, iLen=events.length; i<iLen; i++) {
-			element.addEventListener(events[i], listener, false);
-		}
+	var events = eventNames.split(' ');
+	for (var i=0, iLen=events.length; i<iLen; i++) {
+		element.addEventListener(events[i], listener, false);
 	}
-	
-addListenerMulti(window, 'keyup keypress', countCharacter);
-//document.getElementsByTagName('textarea')[0].addEventListener('keyup', countCharacter);
-//document.getElementsByTagName('textarea')[0].addEventListener('keypress', countCharacter);
+}
 
 function countCharacter()
 {
@@ -64,4 +54,9 @@ function countCharacter()
 
 
 // Light/Dark Mode
+
+
+// Call to functions
+document.getElementById('iptValider').addEventListener('click', repeat);
+//addListenerMulti(window, 'keyup keypress', countCharacter);
 
