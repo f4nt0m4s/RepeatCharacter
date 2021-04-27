75793274ed88e9f453732227fd8f9857a9cea190
Rachid <81814923+R4ch1d-3xyp3ry@users.noreply.github.com>
2021-04-27 18:39:18 +0200

    fix : Inputs error

--- functions.js
+++ functions.js
@@ -1,36 +1,62 @@
 "use strict";
 
+function checkInputsError()
+{
+	let txtArea = document.getElementById('txtAreaId');
+	let counter = document.getElementById('countId');
+
+	if ( txtArea.value === "" || txtArea.value.length == 0 )
+	{
+		txtArea.className  += ' border-3 rounded border-danger ';
+		return false;
+	}
+
+	if ( counter.value === "" || counter.value.length == 0 )
+	{
+		counter.className  += ' border-1 rounded border-danger ';
+		return false;
+	}
+
+	return true;
+}
+
 // Repeat the string or character
 function repeat()
-{
-		console.log('Bouton valider !');
-		// Récupération du texte
-		let txtArea = document.getElementById('txtAreaId');
-		let areaValue = null;
-		// Récupération du compteur
-		let counter = document.getElementById('countId');
-		let counterValue = null;
-		// Valeur à afficher
-		let retValue = ""; //( txtArea == null || txtArea == undefined ) ? "" : txtArea.value;
-		areaValue = ( txtArea == null || txtArea == undefined ) ? "" : txtArea.value;
-		counterValue = ( counter == null || counter == undefined || counter.value === "" ) ? 0 : counter.value;
-		
-		let lengthTextTyped = areaValue.length;
-		if ( lengthTextTyped != 0 )
-		{
-			for(let i=0; i<counterValue; i++)
-			{
-				retValue += areaValue;
-			}
+{	
+	if ( !checkInputsError() )
+	{
+		return;
+	}
 
-			let lengthText = retValue.length;
-			console.log(lengthText);
-			if ( lengthText != 0 )
-			{
-				console.log("yes : " + retValue)
-				document.getElementById('txtAreaId').value = retValue;
-			}
+	//console.log('Bouton valider !');
+	// Récupération du texte
+	let txtArea = document.getElementById('txtAreaId');
+	let areaValue = null;
+	// Récupération du compteur
+	let counter = document.getElementById('countId');
+	let counterValue = null;
+
+	// Valeur à afficher
+	let retValue = ""; //( txtArea == null || txtArea == undefined ) ? "" : txtArea.value;
+	areaValue = ( txtArea == null || txtArea == undefined ) ? "" : txtArea.value;
+	counterValue = ( counter == null || counter == undefined || counter.value === "" ) ? 0 : counter.value;
+	
+	let lengthTextTyped = areaValue.length;
+	if ( lengthTextTyped != 0 )
+	{
+		for(let i=0; i<counterValue; i++)
+		{
+			retValue += areaValue;
 		}
+
+		let lengthText = retValue.length;
+		//console.log(lengthText);
+		if ( lengthText != 0 )
+		{
+			//console.log("yes : " + retValue)
+			document.getElementById('txtAreaId').value = retValue;
+		}
+	}
 }
 
 
@@ -58,11 +84,14 @@ function countCharacter()
 }
 
 
-// Copy text
+/** 
+	* Copy text
+	* source : https://www.w3schools.com/howto/howto_js_copy_clipboard.asp
+*/
 function copyText()
 {
-	let copyText = document.getElementById("textarea");
-	  
+	let copyText = document.getElementById("txtAreaId");
+	
 	/* Select the text field */
 	copyText.select();
 	copyText.setSelectionRange(0, 99999); /* For mobile devices */
@@ -70,10 +99,6 @@ function copyText()
 	/* Copy the text inside the text field */
 	document.execCommand("copy");
 }
-
-
-
-
 
 
 // Call to functions
